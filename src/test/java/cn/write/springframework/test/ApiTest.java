ce1c5f73cff21ba64f246102151d2dd452dbf9e6
18381826414 <liyl778@163.com>
2022-12-13 21:05:31 +0800

    重新构建分支

--- src/test/java/cn/write/springframework/test/ApiTest.java
+++ src/test/java/cn/write/springframework/test/ApiTest.java
@@ -2,8 +2,10 @@ package cn.write.springframework.test;
 
 import cn.write.springframework.beans.factory.config.BeanDefinition;
 import cn.write.springframework.BeanFactory;
+import cn.write.springframework.beans.factory.support.DefaultListableBeanFactory;
 import cn.write.springframework.test.bean.UserService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 
 /**
  * @program: write-spring
@@ -15,15 +17,16 @@ public class ApiTest {
     @Test
     public void test_BeanFactory(){
         // 1.初始化 BeanFactory
-        BeanFactory beanFactory = new BeanFactory();
-
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         // 2.注册 bean
-        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
         beanFactory.registerBeanDefinition("userService", beanDefinition);
-
-        // 3.获取 bean
+        // 3.第一次获取 bean
         UserService userService = (UserService) beanFactory.getBean("userService");
         userService.queryUserInfo();
+        // 4.第二次获取 bean from Singleton
+        UserService userService_singleton = (UserService) beanFactory.getBean("userService");
+        userService_singleton.queryUserInfo();
     }
 
 }
