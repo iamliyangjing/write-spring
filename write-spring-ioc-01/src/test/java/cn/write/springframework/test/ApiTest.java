c1c34a68c5bc3c6acb909ad1d9463837440b17c3
cooper.li <958390434@qq.com>
2025-01-12 01:59:04 +0800

    take over

--- write-spring-ioc-01/src/test/java/cn/write/springframework/test/ApiTest.java
+++ write-spring-ioc-01/src/test/java/cn/write/springframework/test/ApiTest.java
@@ -2,6 +2,7 @@ package cn.write.springframework.test;
 
 import cn.write.springframework.beans.factory.config.BeanDefinition;
 import cn.write.springframework.BeanFactory;
+import cn.write.springframework.beans.factory.support.DefaultListableBeanFactory;
 import cn.write.springframework.test.bean.UserService;
 import org.junit.jupiter.api.Test;
 
@@ -15,15 +16,19 @@ public class ApiTest {
     @Test
     public void test_BeanFactory(){
         // 1.初始化 BeanFactory
-        BeanFactory beanFactory = new BeanFactory();
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
         // 2.注册 bean
-        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
         beanFactory.registerBeanDefinition("userService", beanDefinition);
 
         // 3.获取 bean
         UserService userService = (UserService) beanFactory.getBean("userService");
         userService.queryUserInfo();
+
+        //4.获取bean
+        UserService userService_sig = (UserService) beanFactory.getBean("userService");
+        userService_sig.queryUserInfo();
     }
 
 }
